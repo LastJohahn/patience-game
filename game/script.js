d8ba058b5539f4e6b066d04cc4d7b4148f58cb59
Johann Hannerth <hannerth94@gmail.com>
2021-11-15 16:20:52 +0000

    add startGame function that creates a deck and splits it into cards in hand and cards to deal

--- game/script.js
+++ game/script.js
@@ -1,3 +1,12 @@
 import Deck from "./deck.js";
 
-const deck = new Deck();
+let wholeDeck, dealDeck, inHand;
+
+startGame();
+
+function startGame() {
+  wholeDeck = new Deck();
+  wholeDeck.shuffle();
+  dealDeck = new Deck(wholeDeck.cards.slice(0, 50));
+  inHand = new Deck(wholeDeck.cards.slice(50, wholeDeck.numberOfCards));
+}
