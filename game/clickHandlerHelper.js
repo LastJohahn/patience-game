5c3254013e39d1eac18845a05484c6b7fe1c7c2e
LastJohahn <hannerth94@gmail.com>
2022-02-18 12:52:51 +0000

    create noClickHere function to export

---
+++ game/clickHandlerHelper.js
@@ -0,0 +1,13 @@
+function noClickHere(event) {
+    if (
+        !event.target.classList.contains("is-open") &&
+        !event.target.classList.contains("ace-stack") &&
+        !event.target.classList.contains("card-stack") &&
+        !event.target.classList.contains("is-flipped") &&
+        !event.target.classList.contains("deck-stack")
+      ) {
+        return;
+      }
+}
+
+export {noClickHere}
